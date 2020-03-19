4995486cffa059e0a7f5c6e56bed5204381973e7
Jake Fitzgerald <jakefitz@whatsapp.com>
2020-03-19 13:38:48 -0700

    emotion tracking

--- App.js
+++ App.js
@@ -2,17 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import Emoji from 'react-native-emoji';
-
-const EMOJIS = [
-  "joy",
-  "stuck_out_tongue_closed_eyes",
-  "rage",
-  "scream",
-  "sunglasses"
-]
+import * as FaceDetector from 'expo-face-detector';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null)
+
+  const [faces, setFaces] = useState({})
 
   useEffect(() => {
     (async () => {
@@ -28,19 +23,50 @@ export default function App() {
     return <Text>No access to camera</Text>
   }
 
+  const faceValues = Object.values(faces)
+
   return (
     <View style={{ flex: 1 }}>
       <Camera
         style={{ flex: 1 }}
         type={Camera.Constants.Type.front}
+        onFacesDetected={({faces}) => {
+          const newFaces = {}
+          faces.forEach(face => {
+            let emoji = null;
+            const smiling = face.smilingProbability
+            const eyesOpen = (face.leftEyeOpenProbability + face.rightEyeOpenProbability)/2
+            if (smiling >= 0.5) {
+              emoji = "grinning"
+            } else if (smiling < 0.5) {
+              emoji = "neutral_face"
+              if (eyesOpen < 0.25) {
+                emoji = "rage"
+              }
+            }
+            newFaces[face.faceID] = {
+              style: {
+                left: face.bounds.origin.x,
+                top: face.bounds.origin.y,
+                fontSize: (face.bounds.size.width + face.bounds.size.height)/2
+              },
+              emoji: emoji
+            }
+          })
+          setFaces(newFaces)
+        }}
+        faceDetectorSettings={{
+          mode: FaceDetector.Constants.Mode.fast,
+          detectLandmarks: FaceDetector.Constants.Landmarks.none,
+          runClassifications: FaceDetector.Constants.Classifications.all,
+          minDetectionInterval: 100,
+          tracking: true,
+        }}
       >
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-        </View>
+        {faceValues.length > 0 && <Emoji name={faceValues[0].emoji} style={{
+          position: 'absolute',
+          ...faceValues[0].style
+        }} />}
       </Camera>
     </View>
   )
